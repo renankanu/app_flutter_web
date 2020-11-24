9fac5b7efc36c6262a6f860458c0552f4ec714db
renankanu <renankanu@gmail.com>
2020-11-23 23:44:17 -0300

    Feature - create default button

--- lib/screens/home/widgets/app_bar.dart
+++ lib/screens/home/widgets/app_bar.dart
@@ -1,3 +1,4 @@
+import 'package:app_flutter_web/constant.dart';
 import 'package:flutter/material.dart';
 
 import 'item_menu.dart';
@@ -40,7 +41,27 @@ class CustomAppBar extends StatelessWidget {
           ItemMenu(title: 'pricing', onTap: () {}),
           ItemMenu(title: 'contact', onTap: () {}),
           ItemMenu(title: 'login', onTap: () {}),
+          DefaultButton()
         ],
+      ),
+    );
+  }
+}
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: FlatButton(
+        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+        color: kPrimaryColor,
+        onPressed: () {},
+        child: Text('Get Started'.toUpperCase()),
       ),
     );
   }
