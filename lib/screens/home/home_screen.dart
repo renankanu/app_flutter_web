2658fcb72e3d577acd32a570f7904c8fc5071043
renankanu <renankanu@gmail.com>
2020-11-23 22:55:28 -0300

    Feature - add image background[error]

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size _size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: _size.height,
+        width: _size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
